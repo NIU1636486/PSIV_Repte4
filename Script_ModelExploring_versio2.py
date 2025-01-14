8f7a738e470e63184c2099ab64b0c1685210d25d
Pol Riubrogent <senshiprc@gmail.com>
2025-01-14 10:38:32 +0100

    mes proves

--- Script_ModelExploring_versio2.py
+++ Script_ModelExploring_versio2.py
@@ -22,10 +22,14 @@ if platform.system() == 'Linux':
 else:
     DATA_PATH = "input_reduit"
 
+WANDB_SET = False
 
-
+if WANDB_SET:
+    import wandb
+    wandb.login(key="8e9b2ed0a8b812e7888f16b3aa28491ba440d81a")
+    wandb.init(project="PSIV_Repte4", config={"epochs": epochs, "batch_size": batch_size}, dir="wandb")
 ### DEFINE VARIABLES
-DEVICE = 'cpu'       # options: 'cpu', 'cuda:0', 'cuda:1'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")       # options: 'cpu', 'cuda:0', 'cuda:1'
 N_CLASSES = 2        # number of classes. This case 2={seizure ,non-seizure}
 
 # Default hyper parameters
@@ -114,6 +118,8 @@ for epoch in range(epochs):
 
     for X_batch, y_batch in dataloader:
         print(X_batch.shape)
+        X_batch = X_batch.to(device)
+        y_batch = y_batch.to(device)
         
         # Reorganitzar les dimensions per la LSTM: [batch, sequence_length, features]
         # com les linies comentades anteriorment de execute a lstm unit
@@ -130,8 +136,17 @@ for epoch in range(epochs):
         _, predicted = torch.max(outputs, 1)
         correct += (predicted == y_batch).sum().item()
         total += y_batch.size(0)
-
+        if WANDB_SET:
+            wandb.log({"loss": loss.item(), "accuracy": correct / total})
+    
+    if WANDB_SET:
+        wandb.log({"loss": total_loss / len(dataloader), "accuracy": correct / total, "epoch": epoch})
     print(f"Epoch {epoch + 1}/{epochs}, Loss: {total_loss / len(dataloader):.4f}, Accuracy: {correct / total:.4f}")
 
 # Guardar el modelo
+model = model.to("cpu")
 torch.save(model.state_dict(), "epilepsy_lstm.pth")
+print("Model saved")
+if WANDB_SET:
+    wandb.finish()
+
