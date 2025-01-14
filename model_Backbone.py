8bb4d2dcd8f9344064419901c8c1e45dec2bb1b4
Pol Riubrogent <senshiprc@gmail.com>
2025-01-14 16:11:56 +0100

    test for backbone feature extraction

--- model_Backbone.py
+++ model_Backbone.py
@@ -10,13 +10,15 @@ class CNNModel(nn.Module):
     def __init__(self):
         super(CNNModel, self).__init__()
 
-        self.n_channels = 21 
-        self.n_classes = 2 
+        self.n_channels = 21
+        self.n_classes = 2
 
         self.kernel_sizes = [5, 3, 3] 
         self.pool_sizes = [2, 2, 2]  
 
         self.output_channels = [16, 32, 64]
+
+        self.features = 64
 
         self.blocks = nn.ModuleList()
         in_channels = 1 
@@ -35,28 +37,23 @@ class CNNModel(nn.Module):
 
         self.fusion = nn.Flatten(start_dim=2)
 
-        self.output_unit = nn.Linear(self.output_channels[-1], self.n_classes)
+        self.fc = nn.Linear(self.output_channels[-1], self.features)
+        
+        self.output_unit = nn.Linear(self.features, self.n_classes)
 
-        # self.output = nn.Linear(64, self.n_classes)
-
-    def forward(self, x, feature_extraction=True):
-        x = x.unsqueeze(1) 
+    def forward(self, x):
+        x = x.unsqueeze(1)
 
         for block in self.blocks:
             x = block(x) 
-        if feature_extraction:
-            # x = x.mean(dim=2)  # Now [batch_size, 64, 14]
-            x = x.mean(dim=2)  # This will reduce the sequence length dimension
-            # x = x.squeeze(2)   # Remove the sequence length dimension (now we have a [batch_size, 64] tensor)
-            print("SHAPE IN", x.shape)
-
-            return x
 
         x = self.fusion(x)
+
         x = x.mean(dim=2)
+        x = self.fc(x)
+
+        print("x.shape after fc", x.shape)
+        
         x = self.output_unit(x)
 
-        if feature_extraction:
-            return x
-        
         return x
